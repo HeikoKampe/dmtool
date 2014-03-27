554d6d8d33be64c2631d9bdec571c8d790762630
Heiko Kampe <kampe@einzelkind.com>
2014-03-27 15:16:49 +0100

    making some test request

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,29 @@
 'use strict';
 
 angular.module('dmtoolApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http, $routeParams) {
+
+    var
+      urlDilax = "https://demodwm3.dilax.com/davisweb/rest/md/raw/vehiclecodes",
+      urlTest = "http://public-api.wordpress.com/rest/v1/sites/wtmpeachtest.wordpress.com/posts",
+      urlTest2 = "http://www.kondespro.de";
+
+    $http.get(urlDilax).success(function (data) {
+      $scope.data1 = data;
+    });
+
+    $http.get(urlTest).success(function (data) {
+      $scope.data2 = data;
+    });
+
+
+    $http.jsonp( urlDilax + '?callback=JSON_CALLBACK' ).then( function ( response ) {
+        $scope.data3 = response.data;
+    });
+
+    $http.jsonp( urlTest + '?callback=JSON_CALLBACK' ).then( function ( response ) {
+        $scope.data4 = response.data;
+    });
+
+
   });
