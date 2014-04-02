41da47466bd3a0ce21967e7112ad89c2e9fa9655
Heiko Kampe <kampe@einzelkind.com>
2014-04-02 19:06:35 +0200

    added API queries for Schedule and Count data to the map view

--- app/scripts/controllers/map.js
+++ app/scripts/controllers/map.js
@@ -4,14 +4,44 @@ angular.module('dmtoolApp')
   .controller('MapCtrl', function ($scope, apiService) {
 
     function getRawData() {
-      console.log("$scope.queryParams: ", $scope.queryParams);
-      apiService.getRawData('vehicletrips', $scope.queryParams).then(function (res) {
-        console.log("$scope.data", res.data);
-        $scope.map.data = res.data;
+      console.log("$scope.queryParamsRaw: ", $scope.queryParamsRaw);
+      apiService.getRawData('vehicletrips', $scope.queryParamsRaw).then(function (res) {
+        console.log("res.data", res.data);
+        $scope.map.rawData = res.data;
       });
     }
 
-    $scope.queryParams = {
+    function getScheduleData() {
+      console.log("$scope.queryParamsSchedule: ", $scope.queryParamsSchedule);
+      apiService.getScheduleData('vehicletrips', $scope.queryParamsSchedule).then(function (res) {
+        console.log("res.data", res.data);
+        $scope.map.scheduleData = res.data;
+      });
+    }
+
+    function getCountData() {
+      console.log("$scope.queryParamsCount: ", $scope.queryParamsCount);
+      apiService.getScheduleData('vehicletrips', $scope.queryParamsCount).then(function (res) {
+        console.log("res.data", res.data);
+        $scope.map.countData = res.data;
+      });
+    }
+
+    $scope.queryParamsRaw = {
+      vehicleCodes: '2806,2827,459019',
+      dateFrom: '2012-01-11',
+      dateTo: '2012-01-12',
+      isStop: true
+    };
+
+    $scope.queryParamsSchedule = {
+      vehicleCodes: '2806,2827,459019',
+      dateFrom: '2012-01-11',
+      dateTo: '2012-01-12',
+      isStop: true
+    };
+
+    $scope.queryParamsCount = {
       vehicleCodes: '2806,2827,459019',
       dateFrom: '2012-01-11',
       dateTo: '2012-01-12',
@@ -20,8 +50,8 @@ angular.module('dmtoolApp')
 
     $scope.map = {
       center: {
-        latitude: 51.00849,
-        longitude: 13.85454
+        latitude: 51.00996573333334,
+        longitude: 13.783468888333335
       },
       zoom: 12,
       options: {
@@ -33,17 +63,30 @@ angular.module('dmtoolApp')
           position: google.maps.ControlPosition.TOP_RIGHT
         }
       },
-      data: []
+      rawData: [],
+      scheduleData: [],
+      countData: []
     };
 
     $scope.redMarker = { icon: '../images/red-dot.png' };
     $scope.blueMarker = { icon: '../images/blue-dot.png' };
     $scope.greenMarker = { icon: '../images/green-dot.png' };
+    $scope.pinkMarker = { icon: '../images/pink-dot.png' };
 
-    $scope.submit = function () {
+    $scope.submitRaw = function () {
       getRawData();
     }
 
+    $scope.submitSchedule = function () {
+      getRawData();
+    }
+
+    $scope.submitCount = function () {
+      getCountData();
+    }
+
     getRawData();
+    getScheduleData();
+    getCountData();
 
   });
