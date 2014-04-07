db35088c6f78bd56e40e0958f1a5bdf498f9dc0a
Heiko Kampe <kampe@einzelkind.com>
2014-04-07 19:40:44 +0200

    added function for setting the map center according to the query result

--- app/scripts/controllers/map2.js
+++ app/scripts/controllers/map2.js
@@ -6,10 +6,21 @@ angular.module('dmtoolApp')
     $scope.mapConf = mapConf;
     $scope.sequences = [];
 
+    function setMapCenter(data) {
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].latitude > 0 && data[i].longitude > 0) {
+          $scope.mapConf.center.latitude = data[i].latitude;
+          $scope.mapConf.center.longitude = data[i].longitude;
+          break;
+        }
+      }
+    }
+
     function getRawData() {
       console.log($routeParams);
       apiService.getRawData('stopstatistics/tripstarttime/' + $routeParams.vehicleId + '/' + $routeParams.startTime).then(function (res) {
         console.log("res.data", res.data);
+        setMapCenter(res.data);
         $scope.sequences = sequenceService.createSequences(res.data, 'cntTripKey');
       });
     }
