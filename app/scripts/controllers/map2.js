76ab42ce43ac962ff3d314e65f52bed081b8a5db
Heiko Kampe <kampe@einzelkind.com>
2014-05-01 19:02:31 +0200

    added block selection for stop sequence view

--- app/scripts/controllers/map2.js
+++ app/scripts/controllers/map2.js
@@ -5,42 +5,82 @@ angular.module('mdToolApp')
 
     $scope.mapConfig = gMapService.getMapConfig();
     $scope.rawStopsSequences = [];
-    $scope.plannedStops = [];
     $scope.plannedStopsSequences = [];
+    $scope.plannedStopsSequencesOfSelectedBlock = [];
+
+    $scope.submitBlockForm = function () {
+      if ($scope.selectedBlock && $scope.selectedBlock.blockKey) {
+        getScheduledStopsOfSelectedBlock($scope.selectedBlock.blockKey);
+      }
+    }
 
     function setMapCenter(data) {
       for (var i = 0; i < data.length; i++) {
         if (data[i].latitude > 0 && data[i].longitude > 0) {
-            gMapService.setMapCenter(data[i].latitude, data[i].longitude);
+          gMapService.setMapCenter(data[i].latitude, data[i].longitude);
           break;
         }
       }
     }
 
-    function setDateTime(timeString) {
-      $scope.dateTime = new Date(parseInt(timeString)).toLocaleString();
+    function setDate(timeString) {
+      var dateArray;
+
+      $scope.date = new Date(parseInt(timeString)).toLocaleDateString();
+      dateArray = $scope.date.split('.');
+      $scope.isoDate = dateArray[2] + '-' + dateArray[1] + '-' + dateArray[0];
     }
 
-    function getRawData() {
-      console.log($routeParams);
+    function findValidTripKey(data) {
+      for (var i = 0; i < data.length; i++) {
+        if (data[i].cntTripKey) {
+          return data[i].cntTripKey;
+        }
+      }
+    }
+
+    function getScheduledStopsOfMatchedBlock(blockKey) {
+      apiService.getScheduleData('stops' + '?operationDay=' + $scope.isoDate + '&blockKey=' + blockKey).then(function (res) {
+        console.log("res.scheduleDataOfMatchedBlock", res.data);
+        $scope.plannedStopsSequencesOfMatchedBlock = sequenceService.createSequences(res.data, 'tripKey');
+      });
+    }
+
+    function getMatchedBlockKey(tripKey) {
+      apiService.getScheduleData('blockkey' + '?cntTripKey=' + tripKey).then(function (res) {
+        console.log("res.blockKey", res.data);
+        $scope.matchedBlockKey = res.data;
+        getScheduledStopsOfMatchedBlock($scope.matchedBlockKey);
+      });
+    }
+
+    function getStopsOfSelectedVehicle() {
       apiService.getRawData('stops/' + $routeParams.vehicleId + '/' + $routeParams.startTime + '?dateFrom=' + $routeParams.dateFrom + '&dateTo=' + $routeParams.dateTo).then(function (res) {
         console.log("res.stopstatisticsData", res.data);
         setMapCenter(res.data);
-        setDateTime($routeParams.startTime);
+        $scope.matchedBlockKey = getMatchedBlockKey(findValidTripKey(res.data));
         $scope.rawStopsSequences = sequenceService.createSequences(res.data, 'cntTripKey');
       });
     }
 
-    function getScheduleData() {
-      console.log($routeParams);
-      apiService.getScheduleData('stops/' + $routeParams.vehicleId + '/' + $routeParams.startTime).then(function (res) {
-        console.log("res.scheduleData", res.data);
-        $scope.plannedStops = res.data;
-        $scope.plannedStopsSequences = sequenceService.createSequences(res.data, 'tripKey');
+    function getBlocksOfDay() {
+      apiService.getScheduleData('blocks' + '?operationDay=' + $scope.isoDate).then(function (res) {
+        console.log("res.blocksOfDay", res.data);
+        $scope.blocksOfDay = res.data;
       });
     }
 
-    getRawData();
-    getScheduleData();
+
+    function getScheduledStopsOfSelectedBlock(blockKey) {
+      apiService.getScheduleData('stops' + '?operationDay=' + $scope.isoDate + '&blockKey=' + blockKey).then(function (res) {
+        console.log("res.scheduleDataOfSelectedBlock", res.data);
+        $scope.plannedStopsSequencesOfSelectedBlock = sequenceService.createSequences(res.data, 'tripKey');
+      });
+    }
+
+    setDate($routeParams.startTime);
+    getStopsOfSelectedVehicle();
+    getBlocksOfDay();
+
 
   });
